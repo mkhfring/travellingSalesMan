dbc6aa0085794c72a56bba2ebea09716cf0c32fc
Zixiao <61948711+zixiaozhao@users.noreply.github.com>
2022-12-07 18:39:02 -0800

    Update SA_openMP.c

--- SA_openMP.c
+++ SA_openMP.c
@@ -9,11 +9,10 @@
 #define T_end (1e-8)        //end temp
 #define q 0.98              //coef
 #define L 1024              //max it
-#define N 12                //number of cities
+#define N 13                //number of cities
 int city_result[N];         //solution
 double city[N][2] = {{48.431245619848255, -123.36537967024275},{49.3021919632101, -123.12226115279474},{53.62208783069036, -113.40039303223175},{52.19013778041407, -106.55403987156726},{50.46323151354986, -104.61637921649017},{66.24852413463175, -128.6448217733023},{49.88727582165454, -97.22962773518263}
-,{43.680419790975435, -79.39774675313382},{45.30760994851931, -75.76618887317751},{51.32530434372933, -73.22678830471655},{64.34788639923939, -95.9939920711135},{69.368278920504, -124.12344886103753}};
-//double city[N][2] = {{9932, 4439}, {10109, 4351}, {11552, 3472}, {10302, 3290}, {8776, 3333}, {7040, 4867}, {9252, 4278}, {9395, 4539}, {11101, 2540}, {9825, 5087}, {10047, 4879}, {10227, 4648}, {100027, 4229}, {9878, 4211}, {9087, 4065}, {10438, 4075}, {10382, 3865}, {11196, 3563}, {11075, 3543}, {11544, 3365}, {11915, 2900}, {11305, 3189}, {11073, 3137}, {10950, 3394}, {11576, 2575}, {12239, 2785}, {11529, 2226}, {9328, 4006}, {10012, 3811}, {9952, 3410}, {10612, 2954}, {10349, 2784}, {11747, 2469}, {11673, 2461}};
+,{43.680419790975435, -79.39774675313382},{45.30760994851931, -75.76618887317751},{51.32530434372933, -73.22678830471655},{64.34788639923939, -95.9939920711135},{69.368278920504, -124.12344886103753},{61.86259591869711, -121.35760613313185}};
 double haversine(double *city1, double *city2); //distance between two city
 double path(int city_result[N]);               //calculate distance of the solution
 void init();                                   //init solution
@@ -31,23 +30,23 @@ int main()
     omp_set_num_threads(8);
     init();
     T = T_start;
+    int j;
+    int result[N];
+    int result_copy[N];
+    double d1;
+    double d2;
     while (T > T_end)
     {
-		#pragma omp parallel
+		#pragma omp parallel private(j, result, result_copy, d1, d2)
     	{
-    		int result[N];
-    		int j;
-    		int result_copy[N];
-    		double p1;
-    		double p2;
     		memcpy(result, city_result, N * sizeof(int));
     		for(j = 0; j < L/8; j++)
     		{
     			memcpy(result_copy, result, N * sizeof(int));
     			creat(result);
-				p1 = path(result_copy); //old
-				p2 = path(result); //new
-				double d = p2 - p1;
+				d1 = path(result_copy); //old
+				d2 = path(result); //new
+				double d = d2 - d1;
 				if(d >= 0)
 				{
 					double r = rand() / (RAND_MAX);
@@ -57,15 +56,16 @@ int main()
 					}
 				}
     		}
-    		p1 = path(result);
-    		p2 = path(city_result);
-    		if(p1 < p2)
+    		d1 = path(result);
+    		#pragma omp critical
+    		d2 = path(city_result);
     		{
-				#pragma omp critical
-    			{
-    			memcpy(city_result, result,N * sizeof(int));
-    			}
-    		}
+				if(d1 < d2)
+				{
+
+					memcpy(city_result, result,N * sizeof(int));
+				}
+			}
     	}
     	T *= q;
     	count++;
@@ -79,8 +79,7 @@ int main()
         printf("%d->", city_result[i]);
     }
     printf("%d\n", city_result[0]);
-    printf("total time taken %fs.\n",time_sum);
-    system("pause");
+    printf("total time taken %0.3fs.\n",time_sum);
     return 0;
 }
 
